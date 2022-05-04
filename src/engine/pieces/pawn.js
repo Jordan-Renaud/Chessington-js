70ca6b99c95aabdd42d60585f441e82dcfe0b98f
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-05-04 13:25:29 +0100

    add pawn moves for first move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,18 +1,33 @@
-import Player from '../player';
-import Square from '../square';
-import Piece from './piece';
+import Player from "../player";
+import Square from "../square";
+import Piece from "./piece";
 
 export default class Pawn extends Piece {
-    constructor(player) {
-        super(player);
+  constructor(player) {
+    super(player);
+  }
+
+  getAvailableMoves(board) {
+    let location = board.findPiece(this);
+
+    //handle first move
+    if (this.player === Player.WHITE && location.row === 1) {
+      return [
+        Square.at(location.row + 1, location.col),
+        Square.at(location.row + 2, location.col),
+      ];
+    } else if (this.player === Player.BLACK && location.row === 6) {
+      return [
+        Square.at(location.row - 1, location.col),
+        Square.at(location.row - 2, location.col),
+      ];
     }
 
-    getAvailableMoves(board) {
-        let location = board.findPiece(this)
-        if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
-        } else {
-            return Square.at(location.row - 1, location.col)
-        }
+    //handle all other moves
+    if (this.player === Player.WHITE) {
+      return [Square.at(location.row + 1, location.col)];
+    } else {
+      return [Square.at(location.row - 1, location.col)];
     }
+  }
 }
