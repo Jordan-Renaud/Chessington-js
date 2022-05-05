4953105e07b6d05a3c1cfba8becf334eb4e82b13
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-05-05 15:38:58 +0100

    add queen moves

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -14,28 +14,32 @@ export default class Bishop extends Piece {
     for (let i = 0; i <= location.col; i++) {
       const nextSpace = Square.at(location.row + i, location.col - i);
       if (nextSpace.equals(location)) continue;
-      if (nextSpace.col >= 0 && nextSpace.row <= 7) newMoves.push(nextSpace);
+      //if (nextSpace.col >= 0 && nextSpace.row <= 7)
+      newMoves.push(nextSpace);
     }
 
     //northeast
     for (let i = 0; i <= 7 - location.col; i++) {
       const nextSpace = Square.at(location.row + i, location.col + i);
       if (nextSpace.equals(location)) continue;
-      if (nextSpace.col <= 7 && nextSpace.row <= 7) newMoves.push(nextSpace);
+      //if (nextSpace.col <= 7 && nextSpace.row <= 7)
+      newMoves.push(nextSpace);
     }
 
     //southeast
     for (let i = 0; i <= location.row; i++) {
       const nextSpace = Square.at(location.row - i, location.col + i);
       if (nextSpace.equals(location)) continue;
-      if (nextSpace.col <= 7 && nextSpace.row >= 0) newMoves.push(nextSpace);
+      //if (nextSpace.col <= 7 && nextSpace.row >= 0)
+      newMoves.push(nextSpace);
     }
 
     //southwest
     for (let i = 0; i <= location.row; i++) {
       const nextSpace = Square.at(location.row - i, location.col - i);
       if (nextSpace.equals(location)) continue;
-      if (nextSpace.col >= 0 && nextSpace.row >= 0) newMoves.push(nextSpace);
+      //if (nextSpace.col >= 0 && nextSpace.row >= 0)
+      newMoves.push(nextSpace);
     }
 
     return newMoves;
--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,11 +1,59 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Square from "../square";
 
 export default class Queen extends Piece {
-    constructor(player) {
-        super(player);
+  constructor(player) {
+    super(player);
+  }
+
+  getAvailableMoves(board) {
+    let location = board.findPiece(this);
+    let newMoves = [];
+
+    //northwest
+    for (let i = 0; i <= location.col; i++) {
+      const nextSpace = Square.at(location.row + i, location.col - i);
+      if (nextSpace.equals(location)) continue;
+      //if (nextSpace.col >= 0 && nextSpace.row <= 7)
+      newMoves.push(nextSpace);
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+    //northeast
+    for (let i = 0; i <= 7 - location.col; i++) {
+      const nextSpace = Square.at(location.row + i, location.col + i);
+      if (nextSpace.equals(location)) continue;
+      //if (nextSpace.col <= 7 && nextSpace.row <= 7)
+      newMoves.push(nextSpace);
     }
+
+    //southeast
+    for (let i = 0; i <= location.row; i++) {
+      const nextSpace = Square.at(location.row - i, location.col + i);
+      if (nextSpace.equals(location)) continue;
+      //if (nextSpace.col <= 7 && nextSpace.row >= 0)
+      newMoves.push(nextSpace);
+    }
+
+    //southwest
+    for (let i = 0; i <= location.row; i++) {
+      const nextSpace = Square.at(location.row - i, location.col - i);
+      if (nextSpace.equals(location)) continue;
+      //if (nextSpace.col >= 0 && nextSpace.row >= 0)
+      newMoves.push(nextSpace);
+    }
+
+    //vertical move
+    for (let i = 0; i <= 7; i++) {
+      if (i !== location.row) {
+        newMoves.push(Square.at(i, location.col));
+      }
+    }
+    //horizontal move
+    for (let i = 0; i <= 7; i++) {
+      if (i !== location.col) {
+        newMoves.push(Square.at(location.row, i));
+      }
+    }
+    return newMoves;
+  }
 }
