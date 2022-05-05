687ce87643d806e5b4354e39b317c1920cf243b2
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-05-05 13:27:28 +0100

    add bishop moving diagonally

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,11 +1,43 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Square from "../square";
 
 export default class Bishop extends Piece {
-    constructor(player) {
-        super(player);
+  constructor(player) {
+    super(player);
+  }
+
+  getAvailableMoves(board) {
+    let location = board.findPiece(this);
+    let newMoves = [];
+
+    //northwest
+    for (let i = 0; i <= location.col; i++) {
+      const nextSpace = Square.at(location.row + i, location.col - i);
+      if (nextSpace.equals(location)) continue;
+      if (nextSpace.col >= 0 && nextSpace.row <= 7) newMoves.push(nextSpace);
     }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+    //northeast
+    for (let i = 0; i <= 7 - location.col; i++) {
+      const nextSpace = Square.at(location.row + i, location.col + i);
+      if (nextSpace.equals(location)) continue;
+      if (nextSpace.col <= 7 && nextSpace.row <= 7) newMoves.push(nextSpace);
     }
+
+    //southeast
+    for (let i = 0; i <= location.row; i++) {
+      const nextSpace = Square.at(location.row - i, location.col + i);
+      if (nextSpace.equals(location)) continue;
+      if (nextSpace.col <= 7 && nextSpace.row >= 0) newMoves.push(nextSpace);
+    }
+
+    //southwest
+    for (let i = 0; i <= location.row; i++) {
+      const nextSpace = Square.at(location.row - i, location.col - i);
+      if (nextSpace.equals(location)) continue;
+      if (nextSpace.col >= 0 && nextSpace.row >= 0) newMoves.push(nextSpace);
+    }
+
+    return newMoves;
+  }
 }
