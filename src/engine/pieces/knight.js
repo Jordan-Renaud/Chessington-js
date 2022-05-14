5d01533006a9b5e8022b2515fa8d4e93a7d49f3e
Jordan-Renaud <jordan.dj.renaud@gmail.com>
2022-05-14 13:12:59 +0100

    add knight moves

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,11 +1,37 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Square from "../square";
 
 export default class Knight extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
 
-    getAvailableMoves(board) {
-        return new Array(0);
-    }
+  getAvailableMoves(board) {
+    let location = board.findPiece(this);
+    let newMoves = [];
+    const checkIfInBoard = (num) => num <= 7 && num >= 0;
+
+    const newCoordsForPiece = [
+      //northwest
+      [location.row + 2, location.col + 1],
+      [location.row + 1, location.col + 2],
+      //southwest
+      [location.row - 1, location.col + 2],
+      [location.row - 2, location.col + 1],
+      //southeast
+      [location.row - 2, location.col - 1],
+      [location.row - 1, location.col - 2],
+      //northeast
+      [location.row + 1, location.col - 2],
+      [location.row + 2, location.col - 1],
+    ];
+
+    newCoordsForPiece.forEach((coord) => {
+      if (checkIfInBoard(coord[0]) && checkIfInBoard(coord[1])) {
+        newMoves.push(Square.at(coord[0], coord[1]));
+      }
+    });
+
+    return newMoves;
+  }
 }
